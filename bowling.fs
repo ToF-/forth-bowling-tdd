c23f898f37245a472e4989dea8cda14cd25b6045
Christophe Thibaut <cthibauttof@gmail.com>
2026-06-13 10:00:08 +0200

    spare result in a bonus (fake it)

--- bowling.fs
+++ bowling.fs
@@ -1,12 +1,24 @@
 \ bowling.fs
 
 variable score
+variable bonus
 
 \ initialize game state
 : start
-    0 score ! ;
+    0 score !
+    0 bonus ! ;
+
+: collect-bonus ( n -- )
+    bonus @ * score +! ;
+
+: check-bonus ( n -- )
+    7 = if
+        1 bonus !
+    then ;
 
 : add-roll ( n -- )
+    dup collect-bonus
+    dup check-bonus
     score +!  ;
 
 : final-score ( -- n )
--- tests.fs
+++ tests.fs
@@ -4,20 +4,27 @@ require ffl/tst.fs
 require bowling.fs
 
 page
-t{
-     ." no rolls result in score of zero" cr
+t{ ." no rolls result in score of zero" cr
      start
      final-score
      0 ?s
 }t
 
-t{   ." average rolls result in sum of rolls" cr
+t{ ." average rolls result in sum of rolls" cr
     start
     3 add-roll
     6 add-roll
     2 add-roll
     4 add-roll
     final-score 15 ?s
+}t
+
+t{ ." spare results in a bonus" cr
+    start
+    3 add-roll
+    7 add-roll
+    3 add-roll
+    final-score 16 ?s
 }t
 
 tst-get-result
